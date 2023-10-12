b25cd10ea920b573be52dbe61cb5d0aa29af6542
Michal Baran <michal.baran.dev@gmail.com>
2023-10-12 21:43:32 +0200

    Extend tailwind config: font & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#020916',
+        accent: '0091CF',
+      },
+      fontFamily: {
+        'dmSans': ['DM Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
